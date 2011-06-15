9db5dc154974ce7a77209855e2ff473267af5039
rodrigoegoncalves <rodrigoegoncalves@84f432ef-c303-45d7-deef-febf90d97e98>
2011-06-15 05:32:49 +0000

    adicionando funcoes de select, insert, updade e delete para as entidades de banco criadas

--- src/Classes/Rating.java
+++ src/Classes/Rating.java
@@ -4,5 +4,33 @@ import DataBase.Entity;
 
 
 public class Rating extends Entity{
-	//TODO
+	String username;
+	String isbn;
+	String review_id;
+	String value;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getReview_id() {
+		return review_id;
+	}
+	public void setReview_id(String review_id) {
+		this.review_id = review_id;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
